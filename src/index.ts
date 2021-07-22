eab541c83f21edb98d85361587fe97ddfb9b8a79
zplusfour <zgjoniur@gmail.com>
2021-07-22 21:40:50 +0300

    somethings are done, still working on the scanner

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('hello')
+import fs from 'fs';
+import Scanner from './scanner';
+
+const main = () => {
+  const f = process.argv[2];
+  const fd = String(fs.readFileSync(f));
+  const scanner = new Scanner(fd);
+
+  process.stdout.write(scanner.scan());
+};
+
+main()
